44353947e1d5dccc23fca434476c99402ffa70b2
Moling Guo <morning2139@gmail.com>
2026-07-25 21:36:37 -0400

    Restart the development API when server code changes

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,7 +1,7 @@
 const { spawn } = require('node:child_process');
 
 const processes = [
-  spawn(process.execPath, ['server/index.js'], { stdio: 'inherit', env: process.env }),
+  spawn(process.execPath, ['--watch', 'server/index.js'], { stdio: 'inherit', env: process.env }),
   spawn(process.execPath, ['node_modules/vite/bin/vite.js'], {
     stdio: 'inherit',
     env: process.env,
